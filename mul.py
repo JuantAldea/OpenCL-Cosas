1e6749a8f6dd7de3f3fecc8174d7a37f6279cd11
Juan-Antonio <cyberjuen@gmail.com>
2014-07-23 17:45:43 +0000

    Cosas

--- mul.py
+++ mul.py
@@ -3,8 +3,8 @@
 import numpy as np
 import pyopencl as cl
 
-a_np = np.random.random((3,3)).astype(np.float32)
-b_np = np.random.random((3,3)).astype(np.float32)
+a_np = np.random.random((3, 3)).astype(np.float32)
+b_np = np.random.random((3, 3)).astype(np.float32)
 r_np = np.empty_like(a_np)
 
 print(a_np)
@@ -21,24 +21,24 @@ b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 r_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
 
 source = """
-__kernel void mul(__global float *A, __global float *B, __global float *C, const int dim1, const int dim2){
-
+__kernel void mul(__global float *A, __global float *B, __global float *C, const int dim1, const int dim2)
+{
    int j = get_global_id(0);
-   int i = get_global_id(1);
- 
+   int matrix_index = j / 9;
+   int i = j / 3;
+   j = j % 3;
    float sum = 0;
    for (int k = 0; k < dim1; k++) {
-      sum += A[i * dim1 + k] * B[k * dim2 + j];
+      sum += A[matrix_index * 9 + i * dim1 + k] * B[matrix_index * 9 + k * dim2 + j];
    }
  
-   C[i * dim1 + j] = sum;
-//   C[i * dim1 + j] = i * dim1 + j;
+   C[matrix_index * 9 + i * dim1 + j] = sum;
 }
 """
 
 prg = cl.Program(ctx, source).build()
 
-prg.mul(queue, (3, 3), None, a_g, b_g, r_g, np.int32(3), np.int32(3), global_offset=None, wait_for=None, g_times_l=False).wait()
+prg.mul(queue, (9,), None, a_g, b_g, r_g, np.int32(3), np.int32(3), global_offset=None, wait_for=None, g_times_l=False).wait()
 
 cl.enqueue_copy(queue, r_np, r_g)
 
