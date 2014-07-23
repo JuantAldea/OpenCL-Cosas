e1a03420807fce079959201048be45c05bdd8bb3
Juan-Antonio <cyberjuen@gmail.com>
2014-07-23 18:33:04 +0000

    Cosa2

--- mul.py
+++ mul.py
@@ -3,9 +3,18 @@
 import numpy as np
 import pyopencl as cl
 
-a_np = np.random.random((3, 3)).astype(np.float32)
-b_np = np.random.random((3, 3)).astype(np.float32)
-r_np = np.empty_like(a_np)
+# a_np = np.random.random((3, 6)).astype(np.float32)
+# b_np = np.random.random((3, 6)).astype(np.float32)
+# r_np = np.random.random((3, 6)).astype(np.float32)
+
+a = np.identity(3, dtype=np.float32)
+a_np = np.vstack((a, a,))
+b_np = np.zeros_like(a_np)
+b_np = np.random.random(a_np.shape).astype(np.float32)
+#a_np = np.identity(3, dtype=np.float32) 
+#b_np = np.identity(3, dtype=np.float32) 
+r_np = np.zeros_like(a_np)
+
 
 print(a_np)
 print()
@@ -24,9 +33,9 @@ source = """
 __kernel void mul(__global float *A, __global float *B, __global float *C, const int dim1, const int dim2)
 {
    int j = get_global_id(0);
-   int matrix_index = j / 9;
-   int i = j / 3;
-   j = j % 3;
+   int matrix_index =  j / 9;
+   int i = j % 3;
+   j = (j - matrix_index * 9) / 3;
    float sum = 0;
    for (int k = 0; k < dim1; k++) {
       sum += A[matrix_index * 9 + i * dim1 + k] * B[matrix_index * 9 + k * dim2 + j];
@@ -38,7 +47,7 @@ __kernel void mul(__global float *A, __global float *B, __global float *C, const
 
 prg = cl.Program(ctx, source).build()
 
-prg.mul(queue, (9,), None, a_g, b_g, r_g, np.int32(3), np.int32(3), global_offset=None, wait_for=None, g_times_l=False).wait()
+prg.mul(queue, (18, ), None, a_g, b_g, r_g, np.int32(3), np.int32(3), global_offset=None, wait_for=None, g_times_l=False).wait()
 
 cl.enqueue_copy(queue, r_np, r_g)
 
